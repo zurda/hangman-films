b580ceca9be513c09ca37635c95c0f36203e843a
zurda <m.weizman@gmail.com>
2020-01-04 16:22:37 +0000

    Added game over on counter = 0

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ function App() {
     return film.title.indexOf(char) > -1 || film.title.indexOf(char.toLowerCase()) > -1 ? null : setCounter(counter - 1)
   }
 
+  const isGameOver = () => {
+    return counter < 1
+  }
+
   const fetchFilm = async (genre) => {
     setFilm(EMPTY_FILM)
     setGuessedLetters([' '])
@@ -41,29 +45,38 @@ function App() {
     }
     setFilm(result.results[filmPosition]);
   };
+  const gameOver = isGameOver()
+
   useEffect(() => {
     fetchGenres().then(result => setGenres(result.genres));
   }, []);
+  useEffect(() => {
+    if (counter < 1) {
+      setGuessedLetters(allLetters)
+      setRevealed(true)
+    }
+  }, [counter]);
   return (
     <>
       <Header appName={"Hangman"} appDescription={'Guess the film'} />
+
       <GameBoard>
         <div>
           {genres && genres.length > 0 && <Dropdown title="Choose a genre" options={genres} onClick={() => setFilm(EMPTY_FILM)} onChange={setSelectedGenre} />}
           <Button onClick={() => fetchFilm(selectedGenre)}>Get film</Button>
         </div>
+        {gameOver ? <div>Game over!</div> : null}
         {film.title !== '' &&
           <div>
             <HiddenText filmArr={film.title.split('')} guessedLetters={letters} />
             <LettersTray guessedLetters={letters} onClickHandler={onCharClickHandler} />
             <div>Guesses left: {counter}</div>
-            <Button onClick={() => {
-              setGuessedLetters(allLetters)
-              setRevealed(true)
-            }
-            }>Reveal</Button>
-            {isRevealed
-              &&
+            {!isRevealed ?
+              <Button onClick={() => {
+                setGuessedLetters(allLetters)
+                setRevealed(true)
+              }
+              }>Reveal</Button> :
               <>
                 < img src={`http://image.tmdb.org/t/p/w185${film.poster_path}`} alt="Film poster" />
                 {film.overview || film.tagline ? <p>{film.overview || film.tagline}</p> : null}
