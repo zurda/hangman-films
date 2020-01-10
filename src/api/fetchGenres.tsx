9c83087b0e2830c8908e8b1edaba57d810a92974
zurda <m.weizman@gmail.com>
2020-01-10 19:57:06 +0000

    Tidy up

--- src/api/fetchGenres.tsx
+++ src/api/fetchGenres.tsx
@@ -1,9 +1,28 @@
-const API_MOVIEDB_KEY = process.env.REACT_APP_MOVIEDB_API_KEY
+import { getRandom, testFilm, getGenreId } from '../helpers'
 
-const fetchGenres = async () => {
+const API_MOVIEDB_KEY = process.env.REACT_APP_MOVIEDB_API_KEY
+const RESULTS_PAGES = 5
+
+export const fetchGenres = async () => {
   return await fetch(
     `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_MOVIEDB_KEY}&language=en-US`
   ).then((response) => response.json())
 }
 
-export default fetchGenres
+export const fetchFilm = async (genre: { id: number }, genresList: any[], resetCurrentState: VoidFunction, setNewFilm: any) => {
+  resetCurrentState()
+  const genreId = getGenreId(genre, genresList) || { id: 99 }
+  const result = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_MOVIEDB_KEY}&language=en-US&with_genres=${
+    genreId.id
+    }&sort_by=popularity.desc&include_adult=false&include_video=false&page=${getRandom(
+      RESULTS_PAGES
+    ) + 1}`
+  ).then((response) => response.json())
+  let filmPosition = getRandom(result.results.length)
+  while (!testFilm(result.results[filmPosition].title)) {
+    filmPosition = getRandom(result.results.length)
+  }
+  setNewFilm(result.results[filmPosition])
+}
+
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
+
 import { colours } from '../style/shared'
+import HiddenText from '../components/HiddenText'
+import LettersTray from '../components/LettersTray'
+import { allLetters } from '../helpers'
+import { fetchGenres, fetchFilm } from '../api/fetchGenres'
+import {
+  Button,
+  ButtonContainer,
+  RoundButton,
+  FlexResponsive,
+  Dropdown
+} from '../components/GameControllers'
+import FilmInfo from '../components/FilmInfo'
+
+const MAX_ATTEMPTS = 7
+const EMPTY_FILM = { title: '', poster_path: '' }
 
 const Container = styled.div`
   width: 100%;
@@ -18,12 +34,90 @@ const ContentContainer = styled.div`
   margin: 0 auto;
 `
 
+const GameBoard = () => {
+  const [counter, setCounter] = useState(MAX_ATTEMPTS)
+  const [film, setFilm] = useState(EMPTY_FILM)
+  const [genres, setGenres] = useState<any[]>([])
+  const [selectedGenre, setSelectedGenre] = useState({ id: 99 })
+  const [letters, setGuessedLetters] = useState([' '])
+  const [isRevealed, setRevealed] = useState(false)
 
+  const onCharClickHandler = (char: string) => {
+    setGuessedLetters([...letters, char, char.toLowerCase()])
+    return film.title.indexOf(char) > -1 ||
+      film.title.indexOf(char.toLowerCase()) > -1
+      ? null
+      : setCounter(counter - 1)
+  }
 
-const GameBoard = ({ children }: any) => (
-  <Container>
-    <ContentContainer>{children}</ContentContainer>
-  </Container>
-)
+  const onRevealClickHandler = () => {
+    setGuessedLetters(allLetters)
+    setRevealed(true)
+  }
+
+  const resetState = () => {
+    setFilm(EMPTY_FILM)
+    setGuessedLetters([' '])
+    setRevealed(false)
+    setCounter(MAX_ATTEMPTS)
+  }
+
+  useEffect(() => {
+    fetchGenres().then((result) => setGenres(result.genres))
+  }, [])
+  useEffect(() => {
+    if (counter < 1) {
+      setGuessedLetters(allLetters)
+      setRevealed(true)
+    }
+  }, [counter])
+  useEffect(() => {
+    const remainingLetters = film.title
+      .split('')
+      .filter((filmLetter) => letters.indexOf(filmLetter) === -1)
+    if (film.title !== '' && remainingLetters.length === 0) {
+      setRevealed(true)
+    }
+  }, [film.title, letters])
+
+  return (
+    <Container>
+      <ContentContainer>
+        <FlexResponsive>
+          {genres && genres.length > 0 && (
+            <Dropdown
+              options={genres}
+              title=""
+              onClick={() => setFilm(EMPTY_FILM)}
+              onChange={setSelectedGenre}
+            />
+          )}
+          <ButtonContainer>
+            <Button onClick={() => fetchFilm(selectedGenre, genres, resetState, setFilm)}>
+              Hit me!
+            </Button>
+          </ButtonContainer>
+        </FlexResponsive>
+        {film.title !== '' && (
+          <div>
+            <HiddenText filmArr={film.title.split('')} guessedLetters={letters} />
+            <LettersTray
+              guessedLetters={letters}
+              onClickHandler={onCharClickHandler}
+            />
+            <p>{counter > 0 ? `Guesses left: ${counter}` : null}</p>
+            {!isRevealed ? (
+              <RoundButton onClick={onRevealClickHandler}>
+                Reveal
+              </RoundButton>
+            ) : (
+                <FilmInfo film={film} />
+              )}
+          </div>
+        )}
+      </ContentContainer>
+    </Container>
+  )
+}
 
 export default GameBoard
