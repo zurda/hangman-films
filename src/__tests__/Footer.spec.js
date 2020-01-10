4add54fef3820a64247b0e5e9244368d738be885
zurda <m.weizman@gmail.com>
2020-01-10 20:21:17 +0000

    Formatted files

--- src/__tests__/Footer.spec.js
+++ src/__tests__/Footer.spec.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { create } from "react-test-renderer";
+import React from 'react'
+import { create } from 'react-test-renderer'
 import Footer from '../components/Footer'
 
-describe("Button component", () => {
-  test("Matches the snapshot", () => {
-    const footer = create(<Footer />);
-    expect(footer.toJSON()).toMatchSnapshot();
-  });
-});
+describe('Button component', () => {
+  test('Matches the snapshot', () => {
+    const footer = create(<Footer />)
+    expect(footer.toJSON()).toMatchSnapshot()
+  })
+})
